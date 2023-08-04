f90c06cc1a59561fa054bb79715b7662f6885e83
iveke <iven.tkachenkoi@gmail.com>
2023-08-04 11:39:05 +0300

    03.08

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const canvas1 = document.getElementById('canvas1');
+const ctx1 = canvas1.getContext('2d');
+canvas1.width = 600;
+canvas1.height = 600;
+
+const canvas2 = document.getElementById('canvas2');
+const ctx2 = canvas2.getContext('2d');
+canvas2.width = 600;
+canvas2.height = 600;
+
+const canvas3 = document.getElementById('canvas3');
+const ctx3 = canvas3.getContext('2d');
+canvas3.width = 600;
+canvas3.height = 600;
+
+const canvas4 = document.getElementById('canvas4');
+const ctx4 = canvas4.getContext('2d');
+canvas4.width = 600;
+canvas4.height = 600;
+
+const canvas5 = document.getElementById('canvas5');
+const ctx5 = canvas5.getContext('2d');
+canvas5.width = 600;
+canvas5.height = 600;
+
